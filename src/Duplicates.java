8e4d6d3a1c1b0f4e41d9a0bf7c4903c9902376c1
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-12-09 11:37:31 -0500

    Add files via upload

---
+++ src/Duplicates.java
@@ -0,0 +1,44 @@
+
+
+public class Duplicates {
+
+	public static void main(String[] args) {
+
+		// Array to hold 20 ints
+		int[] numbers = new int[10];
+
+		// Fill array with random numbers
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = (int) (Math.random() * 50);
+		}
+
+		// Sort the list so that binary search can work
+		boolean result = findDuplicates(numbers);
+		
+		//Display the results of the method
+		System.out.println("Has duplicates: " + result);
+
+		// Display the list so you can verify your work
+		System.out.print("Array values: [");
+		for (int num : numbers) {
+			System.out.print(num + " ");
+		}
+		System.out.println("]");
+
+	}
+	
+	//POST: returns true if arr contains duplicate values
+	//      and false otherwise
+	public static boolean findDuplicates(int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = i + 1; j < arr.length; j++) {
+				if(arr[i] == arr[j]) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+
+}
